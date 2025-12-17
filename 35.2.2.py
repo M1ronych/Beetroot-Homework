5801dc94b46498f46d848ff54c1df1ebcfe4abf9
M1ronych <74962225+M1ronych@users.noreply.github.com>
2025-12-17 14:15:52 +0200

    Update 35.2.2.py

--- 35.2.2.py
+++ 35.2.2.py
@@ -16,11 +16,29 @@ def fetch_comments(after):
         "sort_type":"created_utc"
     }
 
-    response = requests.get(BASE_URL,params=params,timeout=10)
-    response.raise_for_status()
-    data = response.json()["data"]
+    headers = {
+        "User-Agent":"python-homework-script/1.0"
+    }
 
-    return data
+    response = requests.get(BASE_URL,params=params,headers=headers,timeout=10)
+    if response.status_code==403:
+        print("403 Forbidden - Pushshift відмовив")
+        return []
+
+    if response.status_code != 200:
+        print("HTTP error:",response.status_code)
+        return []
+
+    try:
+        return response.json().get("data",[])
+    except ValueError:
+        print("Answer is not JSON")
+        return []
+    
+    #response.raise_for_status()
+    #data = response.json()["data"]
+
+    #return data
 
 def fetch_range(start_after,max_requests=50):
     all_comments = []
@@ -39,7 +57,7 @@ def fetch_range(start_after,max_requests=50):
 
 def main():
     data = fetch_range(0,max_requests=5)
-    print("Given:", len(data))
+    print(len(data))
 
    # start_times = [0,1_000_000_000,1_300_000_000,1_600_000_000]
    # with Pool(processes=cpu_count()) as pool:
@@ -54,5 +72,5 @@ def main():
     #with open("comments.json","w",encoding="utf-8") as f:
     #    json.dump(all_comments,f,ensure_ascii=False,indent=2)
 
-#if __name__ == "__main__":
-    #main()
+if __name__ == "__main__":
+    main()
